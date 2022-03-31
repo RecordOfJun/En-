90670692498fbc34712bcc0ff98554c5941f9dc0
DESKTOP-5O6JFNI\user <pritty0731@naver.com>
2022-03-31 19:27:01 +0900

    test

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num;
+            num=Console.ReadLine();
+            for(int i = 0; i < int.Parse(num); i++)
+            {
+                for (int j = 0; j < int.Parse(num)-1-i; j++)
+                    Console.Write(" ");
+                for (int j = 0; j < i * 2 + 1; j++)
+                    Console.Write("*");
+                Console.WriteLine("");
+            }
+            for (int i = int.Parse(num)-1; i >=0 ; i--)
+            {
+                for (int j = 0; j < int.Parse(num) - 1 - i; j++)
+                    Console.Write(" ");
+                for (int j = 0; j < i * 2 + 1; j++)
+                    Console.Write("*");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
