ab781b9afcf47ca5751bad4cfe1f91783ebf4968
DESKTOP-5O6JFNI\user <pritty0731@naver.com>
2022-04-04 00:55:36 +0900

    [ADD]틱택토 모양 출력

---
+++ TicTacToe/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Data
+    {
+        public List<string> indexOfSquare = new List<string> { "X", "2", "O", "4", "5", "6", "7", "8", "9" };
+        public Data()
+        {
+        }
+        public void Sqaure(int numOfLine)
+        {
+            int leftSquareNum = numOfLine*3;
+            int middleSquareNum = numOfLine * 3 + 1;
+            int rightSquareNum = numOfLine * 3 + 2;
+            if (numOfLine==0)
+                Console.WriteLine("###########################################");
+            Console.WriteLine("#             #             #             #");
+            Console.WriteLine("#             #             #             #");
+            Console.WriteLine("#      "+indexOfSquare[leftSquareNum] + "      #      " + indexOfSquare[middleSquareNum] + "      #      " + indexOfSquare[rightSquareNum] + "      #");
+            Console.WriteLine("#             #             #             #");
+            Console.WriteLine("#             #             #             #");
+            Console.WriteLine("###########################################");
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new Data();
+            for (int i = 0; i < 3; i++)
+                data.Sqaure(i);
+        }
+    }
+}
