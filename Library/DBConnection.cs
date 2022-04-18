0f92085a1e838175d7bf3e65c26da58999b9e083
DESKTOP-TTBTK31\12 <pritty0731@naver.com>
2022-04-19 04:52:06 +0900

    [ADD]책 관련 쿼리문

--- Library/DBConnection.cs
+++ Library/DBConnection.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using MySql.Data.MySqlClient;
 using Library.Model;
+using Library.View;
 namespace Library
 {
     class DBConnection
     {
         MySqlConnection connection;
         MySqlCommand command;
+        BasicView basicView = new BasicView();
         string query;
         public DBConnection()
         {
@@ -23,6 +25,7 @@ namespace Library
             query += member.Id + "','" + member.Password + "','" + member.Name + "','" + member.PersonalCode + "','" + member.PhoneNumber + "','" + member.Address + "');";
             command = new MySqlCommand(query, connection);
             command.ExecuteNonQuery();
+            connection.Close();
         }
         public void UpdateMember(MemberVO member,string code)
         {
@@ -36,6 +39,7 @@ namespace Library
             query += "where membercode=" + code + ";";
             command = new MySqlCommand(query, connection);
             command.ExecuteNonQuery();
+            connection.Close();
         }
         public void DeleteMember(string code)
         {
@@ -45,6 +49,70 @@ namespace Library
             query += "where membercode=" + code + ";";
             command = new MySqlCommand(query, connection);
             command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void SelectMember(string id,string name, string phone)
+        {
+            connection.Open();
+            query = "";
+            query += "SELECT * from member ";
+            query += "where id like '%" + id + "%' and ";
+            query += "name like '%" + name + "%' and ";
+            query += "phone like '%" + phone + "%'; ";
+            command = new MySqlCommand(query, connection);
+            MySqlDataReader reader=command.ExecuteReader();
+            while (reader.Read())
+            {
+                basicView.MemberInformation(new MemberVO(reader["id"].ToString(), reader["password"].ToString(), reader["name"].ToString(), reader["phone"].ToString(), reader["adress"].ToString(), reader["personalcode"].ToString(), reader["membercode"].ToString()));
+            }
+            connection.Close();
+        }
+        public void InsertBook(BookVO book)
+        {
+            connection.Open();
+            query = "";
+            query += "Insert into book values ('";
+            query += book.Id + "','" + book.Name + "','" + book.Publisher + "','" + book.Author + "'," + book.Price + "," + book.Quantity + ");";
+            command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void UpdateBook(BookVO book)
+        {
+            connection.Open();
+            query = "";
+            query += "Update member Set ";
+            query += "quantity=" + book.Quantity + ",";
+            query += "where id='" + book.Id + "';";
+            command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void DeleteBook(string id)
+        {
+            connection.Open();
+            query = "";
+            query += "delete from member ";
+            query += "where id='" + id + "';";
+            command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void SelectBookr(string name, string author, string publisher)
+        {
+            connection.Open();
+            query = "";
+            query += "SELECT * from book ";
+            query += "where name like '%" + name + "%' and ";
+            query += "author like '%" + author + "%' and ";
+            query += "publisher like '%" + publisher + "%'; ";
+            command = new MySqlCommand(query, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                basicView.MemberInformation(new BookVO(reader["id"].ToString(), reader["name"].ToString(), reader["publisher"].ToString(), reader["author"].ToString(), reader["price"].ToString(), int.Parse(reader["quanity"].ToString())));
+            }
+            connection.Close();
         }
     }
 }
--- Library/System.cs
+++ Library/System.cs
@@ -14,7 +14,7 @@ namespace Library
             //libraryProgram.start();
             MemberVO member = new MemberVO("abcd12345", "qwer1235", "조영", "01055533355", "경기도 군포시 고산로", "9808281111118", "6");
             DBConnection dB = new DBConnection();
-            dB.InsertMember(member);
+            dB.SelectMember("","","");
         }//관리자 아이디:1111111111,비밀번호:9999999999
     }
 }
