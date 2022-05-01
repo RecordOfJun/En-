b45e908ad68c1b640277392da03c0c51353e0a41
DESKTOP-TTBTK31\12 <pritty0731@naver.com>
2022-05-01 23:26:01 +0900

    [ADD]NaverBook 객체 추가,api연동

--- Library/LibrarySystem.cs
+++ Library/LibrarySystem.cs
@@ -10,8 +10,12 @@ namespace Library
 
         static void Main(string[] args)
         {
-            MainMenu libraryProgram = new MainMenu();
-            libraryProgram.start();
+            //MainMenu libraryProgram = new MainMenu();
+            //libraryProgram.start();
+            string[] book = new string[2];
+            book = Console.ReadLine().Split(' ');
+            NaverBook naverBook = new NaverBook();
+            Console.WriteLine(naverBook.GetRequestResult(book[0], book[1]));
         }//관리자 아이디:1111111111,비밀번호:9999999999
     }
 }
---
+++ Library/Model/NaverBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+namespace Library.Model
+{
+    class NaverBook
+    {
+
+        public string GetRequestResult(string bookName,string number)
+        {
+            string result = "";
+            WebRequest request;
+            request = WebRequest.Create("https://openapi.naver.com/v1/search/book.json?query="+bookName+"&display="+number);
+
+            request.Headers.Add("X-Naver-Client-Id", "SsfDUUwgAZmuzt8kSFIg");
+            request.Headers.Add("X-Naver-Client-Secret", "IzA2jM8MfA");
+
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            result = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return result;
+        }
+    }
+}
