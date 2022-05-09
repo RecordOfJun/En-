5a83bc908b1beaf50cae767fa3e9020232e38f3e
DESKTOP-5O6JFNI\user <pritty0731@naver.com>
2022-05-09 14:03:41 +0900

    [ADD] 초기 화면 기본적인 UI 생성

--- Java/ImageSearch/src/View/MainFrame.java
+++ Java/ImageSearch/src/View/MainFrame.java
@@ -1,6 +1,5 @@
 package View;
 
-import java.awt.Container;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -11,13 +10,35 @@ public class MainFrame extends JFrame{
 	public void MainForm() {
 		setTitle("이미지 검색");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainContainer.setLayout(new BorderLayout());
 		setSize(1000, 500);
-		setVisible(true);
 	}
 	
 	public void firstForm() {
-		JLabel label=new JLabel("Hello");
-		mainContainer.add(label);
+		JPanel imagePanel=new JPanel();
+		JPanel searchPanel=new JPanel(new FlowLayout());
+		JPanel recordPanel=new JPanel();
+		JLabel googleLabel=new JLabel(new ImageIcon("images/googleLabel.png"));
+		JTextField searchField=new JTextField("검색어를 입력하세요");
+		JButton searchButton=new JButton(new ImageIcon("images/searchIcon.PNG"));
+		JButton recordButton=new JButton("검색기록");
+		
+		mainContainer.setBackground(Color.white);
+		imagePanel.setBackground(Color.white);
+		searchPanel.setBackground(Color.white);
+		recordPanel.setBackground(Color.white);
+		searchButton.setBackground(Color.white);
+		searchButton.setBackground(Color.white);
+		searchButton.setPreferredSize(new Dimension(40,40));
+		searchField.setPreferredSize(new Dimension(300,40));
+		imagePanel.add(googleLabel);
+		searchPanel.add(searchField);
+		searchPanel.add(searchButton);
+		recordPanel.add(recordButton);
+		
+		mainContainer.setLayout(new BorderLayout());
+		mainContainer.add(imagePanel,BorderLayout.NORTH);
+		mainContainer.add(searchPanel,BorderLayout.CENTER);
+		mainContainer.add(recordPanel,BorderLayout.SOUTH);
+		setVisible(true);
 	}
 }
