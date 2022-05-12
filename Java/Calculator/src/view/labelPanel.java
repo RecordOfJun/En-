6ed920cb0d4177b478d5c25e48bf8855d848d1e7
DESKTOP-5O6JFNI\user <pritty0731@naver.com>
2022-05-12 17:19:21 +0900

    [ADD]버튼패널 생성

--- Java/Calculator/src/view/labelPanel.java
+++ Java/Calculator/src/view/labelPanel.java
@@ -1,5 +1,26 @@
 package view;
-
-public class labelPanel {
+import java.awt.*;
+import javax.swing.*;
+public class labelPanel extends JPanel {
+	private JTextField calculateLog=new JTextField("log");
+	private JTextField presentNumber=new JTextField("num");
 	
+	private void setPanel() {
+		this.setLayout(new GridLayout(2,1,0,10));
+		setLogField();
+		setNumberField();
+		this.add(calculateLog);
+		this.add(presentNumber);
+	}
+	
+	private void setLogField() {
+		calculateLog.setHorizontalAlignment(JTextField.RIGHT);
+		calculateLog.setFont(new Font("돋움",Font.PLAIN,30));
+		calculateLog.setSize(new Dimension(this.WIDTH,30));
+	}
+	private void setNumberField() {
+		calculateLog.setHorizontalAlignment(JTextField.RIGHT);
+		presentNumber.setFont(new Font("돋움",Font.BOLD,60));
+		presentNumber.setSize(new Dimension(this.WIDTH,60));
+	}
 }
--- Java/Calculator/src/view/mainFrame.java
+++ Java/Calculator/src/view/mainFrame.java
@@ -1,4 +1,5 @@
 package view;
+import java.awt.BorderLayout;
 import java.awt.Container;
 
 import javax.swing.*;
@@ -6,11 +7,13 @@ import javax.swing.event.*;
 public class mainFrame extends JFrame {
 	private buttonPanel buttons=new buttonPanel();
 	private Container container=getContentPane();
-	
+	private labelPanel calculatings=new labelPanel();
 	public void loadFrame() {
 		setTitle("계산기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		container.add(buttons);
+		container.setLayout(new BorderLayout());
+		container.add(calculatings,BorderLayout.CENTER);
+		container.add(buttons,BorderLayout.SOUTH);
 		setSize(400, 900);
 		setVisible(true);
 	}
